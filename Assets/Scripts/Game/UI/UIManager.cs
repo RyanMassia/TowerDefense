ab8a39bc4067cb26c44c4c310a8643b8e4b8a1f7
Ryan Massia [Student] <RMassia03@student.sl.on.ca>
2019-11-30 12:25:15 -0500

    Chapter 19; UIManager created!

---
+++ Assets/Scripts/Game/UI/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    //1 references UI Manager for instance 
+    public static UIManager Instance;
+    //2 Reference to addTowerWindow 
+    public GameObject addTowerWindow;
+    //3 Reference TO text 
+    public Text txtGold;
+    public Text txtWave;
+    public Text txtEscapedEnemies;
+
+    //1 Set Instance to the UIManager script
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    //2 Update the gold, wave and escaped enemies Text values
+    private void UpdateTopBar()
+    {
+        txtGold.text = GameManager.Instance.gold.ToString();
+        txtWave.text = "Wave " + GameManager.Instance.waveNumber + " / " + WaveManager.Instance.enemyWaves.Count;
+        txtEscapedEnemies.text = "Escaped Enemies " + GameManager.Instance.escapedEnemies + " / " + GameManager.Instance.maxAllowedEscapedEnemies;
+    }
+    //3  Takes a Tower Slot as a parameter, passes it along to the Add Tower Window ﬁeld and shows it at the position of the slot 
+    public void ShowAddTowerWindow(GameObject towerSlot)
+    {
+        addTowerWindow.SetActive(true);
+        addTowerWindow.GetComponent<AddTowerWindow>().towerSlotToAddTowerTo = towerSlot;
+        UtilityMethods.MoveUiElementToWorldPosition(addTowerWindow.GetComponent<RectTransform>(), towerSlot.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTopBar();
+    }
+}
